0ff6f4b725d11fb4370a21effd1804e38ef449c7
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-02-10 12:29:27 -0600

    Adding form to twitter, using columns, adding fa icons and add User model with devise

--- app/controllers/tweeets_controller.rb
+++ app/controllers/tweeets_controller.rb
@@ -3,7 +3,8 @@ class TweeetsController < ApplicationController
 
   # GET /tweeets or /tweeets.json
   def index
-    @tweeets = Tweeet.all
+    @tweeets = Tweeet.all.order("created_at DESC")
+    @tweeet = Tweeet.new
   end
 
   # GET /tweeets/1 or /tweeets/1.json
@@ -25,7 +26,7 @@ class TweeetsController < ApplicationController
 
     respond_to do |format|
       if @tweeet.save
-        format.html { redirect_to @tweeet, notice: "Tweeet was successfully created." }
+        format.html { redirect_to root_path, notice: "Tweeet was successfully created." }
         format.json { render :show, status: :created, location: @tweeet }
       else
         format.html { render :new, status: :unprocessable_entity }
