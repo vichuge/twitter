b5ea0b98275d39591c5bcdf710d4c1d1d4b4b75b
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-02-10 16:30:13 -0600

    Fixing with rubocop

--- app/controllers/tweeets_controller.rb
+++ app/controllers/tweeets_controller.rb
@@ -1,35 +1,33 @@
 class TweeetsController < ApplicationController
-  before_action :set_tweeet, only: %i[ show edit update destroy ]
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_tweeet, only: %i[show edit update destroy]
+  before_action :authenticate_user!, except: %i[index show]
 
   # GET /tweeets or /tweeets.json
   def index
-    @tweeets = Tweeet.all.order("created_at DESC")
+    @tweeets = Tweeet.all.order('created_at DESC')
     @tweeet = Tweeet.new
   end
 
   # GET /tweeets/1 or /tweeets/1.json
-  def show
-  end
+  def show; end
 
   # GET /tweeets/new
   def new
-    #@tweeet = Tweeet.new
+    # @tweeet = Tweeet.new
     @tweeet = current_user.tweeets.build
   end
 
   # GET /tweeets/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /tweeets or /tweeets.json
   def create
-    #@tweeet = Tweeet.new(tweeet_params)
+    # @tweeet = Tweeet.new(tweeet_params)
     @tweeet = current_user.tweeets.build(tweeet_params)
 
     respond_to do |format|
       if @tweeet.save
-        format.html { redirect_to root_path, notice: "Tweeet was successfully created." }
+        format.html { redirect_to root_path, notice: 'Tweeet was successfully created.' }
         format.json { render :show, status: :created, location: @tweeet }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -42,7 +40,7 @@ class TweeetsController < ApplicationController
   def update
     respond_to do |format|
       if @tweeet.update(tweeet_params)
-        format.html { redirect_to @tweeet, notice: "Tweeet was successfully updated." }
+        format.html { redirect_to @tweeet, notice: 'Tweeet was successfully updated.' }
         format.json { render :show, status: :ok, location: @tweeet }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -55,19 +53,20 @@ class TweeetsController < ApplicationController
   def destroy
     @tweeet.destroy
     respond_to do |format|
-      format.html { redirect_to tweeets_url, notice: "Tweeet was successfully destroyed." }
+      format.html { redirect_to tweeets_url, notice: 'Tweeet was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_tweeet
-      @tweeet = Tweeet.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def tweeet_params
-      params.require(:tweeet).permit(:tweeet)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_tweeet
+    @tweeet = Tweeet.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def tweeet_params
+    params.require(:tweeet).permit(:tweeet)
+  end
 end
